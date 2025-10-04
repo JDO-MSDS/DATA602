52f8c6730676f75d7586255c6128a163602337fc
JDO-MSDS <joao.deoliveira50@spsmail.cuny.edu>
2025-10-04 15:58:02 -0400

    Add files via upload

---
+++ assignment3/data602_week3.py
@@ -0,0 +1,7 @@
+animals = ["cat", "dog", "manta ray", "horse", "crouching tiger"]
+
+len_animals = len(animals)
+
+# loop to print each element of the list
+for i in range(len_animals):
+    print(animals[i])
